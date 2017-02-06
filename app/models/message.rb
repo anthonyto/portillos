083c60b06c1f8649586e5f47a14e14721d83412b
Anthony To <to.anthony93@gmail.com>
2017-02-06 12:33:22 -0800

    Don't raise error on rescue. That's dumb.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ActiveRecord::Base
-  
+
   scope :from_other_numbers, -> { where("from_number NOT LIKE '+18582390241'")}
-  
+
   def self.outbound(to)
     portillos_image = PortillosPicker.new.options.sample
     begin
@@ -12,8 +12,7 @@ class Message < ActiveRecord::Base
       })
       Message.create(from_number: '+18582390241', to_number: to, body: 'testing', media_url: 'portillos_image')
     rescue => e
-      raise e
     end
   end
-  
+
 end
