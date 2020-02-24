61d92cfed1fc8b501388d2e6d4c56dac6ef6f745
Anthony To <toanthony93@gmail.com>
2020-02-24 06:39:02 -1000

    Update numbers and renew twilio number

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,15 @@
 class MessagesController < ApplicationController
   # TODO read more about this
   skip_before_action :verify_authenticity_token, only: [:inbound]
-  
+
   def index
     @messages = Message.from_other_numbers
   end
-  
+
   def new
     @message = Message.new
   end
-  
+
   def outbound
     begin
       Message.outbound(message_params[:to_number])
@@ -19,22 +19,22 @@ class MessagesController < ApplicationController
     end
     redirect_to messages_path
   end
-  
+
   def inbound
     Message.create(
       from_number: inbound_message_params[:From],
-      to_number: '+18582390241',
+      to_number: ENV["twilio_number"],
       body: inbound_message_params[:Body]
     )
     render nothing: true
   end
-  
+
   private
-  
+
   def message_params
     params.require(:message).permit(:to_number)
   end
-  
+
   def inbound_message_params
     params.permit(:From, :Body)
   end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,16 +1,16 @@
 class Message < ActiveRecord::Base
 
-  scope :from_other_numbers, -> { where("from_number NOT LIKE '+18582390241'")}
+  scope :from_other_numbers, -> { where("from_number NOT LIKE '#{ENV["twilio_number"]}'")}
 
   def self.outbound(to)
     portillos_image = PortillosPicker.new.options.sample
     begin
       TwilioClient.client.account.messages.create({
         to: to,
-        from: '+18582390241',
+        from: ENV["twilio_number"],
         media_url: portillos_image
       })
-      Message.create(from_number: '+18582390241', to_number: to, body: 'testing', media_url: 'portillos_image')
+      Message.create(from_number: ENV["twilio_number"], to_number: to, body: 'testing', media_url: 'portillos_image')
     rescue => e
     end
   end
